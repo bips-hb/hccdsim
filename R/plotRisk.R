8ecead55a8f79a5e40c4db7bf222bcc593b0fe11
Louis Dijkstra <louisdijkstra (at) gmail.com>
2019-02-20 13:53:46 +0100

    Extended the plotting function

--- R/plotRisk.R
+++ R/plotRisk.R
@@ -6,12 +6,30 @@
 #' @param drug_prescriptions Binary vector denoting the 
 #'                    drug prescription history
 #' @param risk_function One of the risk models 
+#' @param title Title of the plot
 #' @param ylim The limits of the y-axis: \code{c(ymin, ymax)}
 #' 
 #' @export
-plot_risk <- function(drug_prescriptions, risk_function = risk_immediate(), ylim = c(0,1)) { 
+plot_risk <- function(drug_prescriptions, risk_function = risk_immediate(), title = "", ylim = c(0,1)) { 
   risks <- sapply(1:length(drug_prescriptions), function(i)
     risk_function(x[1:i]))
   
-  plot(1:length(drug_prescriptions), risks, ylim = ylim)
+  data <- data.frame(
+    x = 1:length(drug_prescriptions), 
+    y = risks
+  )
+  
+  changes <- data.frame(changes = which(diff(drug_prescriptions) != 0) + .5)
+  
+  ggplot() + 
+    geom_point(data = data, mapping = aes(x = x, y = y)) +
+    geom_vline(data = changes, aes(xintercept = changes), linetype='dashed', color = "red") + 
+    xlab("time") + 
+    ylab("risk") + 
+    scale_x_continuous(limits = c(0.5,length(drug_prescriptions)+.5), expand = c(0, 0)) +
+    scale_y_continuous(limits = c(0,1), expand = c(.01, .01)) + 
+    theme(panel.grid.major = element_blank(), panel.grid.minor = element_blank(),
+          panel.background = element_blank(), axis.line = element_line(colour = "black")) + 
+    ggtitle(title) 
+
 }
--- sandbox.R
+++ sandbox.R
@@ -1,7 +1,24 @@
 library(hccdsim)
 
-x <- c(rep(1,4),0, 0, rep(1, 4), 0, rep(1,4), rep(0,20), rep(1,3))
+x <- c(0, 0, rep(1,10), rep(0, 10))
+drug_prescriptions <- x
+x
+t <- which(x == 1)
+t
 plot_risk(x, risk_function = risk_increase_decrease(6))
+
+indices <- i 
+
+plot_risk(x, risk_function = risk_withdrawal(.4))
+plot_risk(x, risk_function = risk_immediate())
+plot_risk(x, risk_function = risk_long_time_after(.4, 30))
+plot_risk(x, risk_function = risk_increase_decrease(12))
+
+x
+which(diff(x) != 0)
+
+diff(i)
+which(diff(i) > 1)
 
 plot_risk(x, risk_function = risk_withdrawal(1))
 plot_risk(x, risk_function = risk_withdrawal(.5))
